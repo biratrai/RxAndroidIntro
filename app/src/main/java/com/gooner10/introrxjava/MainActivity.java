e7f2cda7ba4d4ee03383df9c7ad0b019fdc882c4
Birat Rai <biratrai@hotmail.com>
2018-11-06 19:00:32 -0500

    cleaning import and adding comments

--- app/src/main/java/com/gooner10/introrxjava/MainActivity.java
+++ app/src/main/java/com/gooner10/introrxjava/MainActivity.java
@@ -1,30 +1,14 @@
 package com.gooner10.introrxjava;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.TextView;
 
-import com.google.gson.Gson;
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.Response;
-
-import java.io.IOException;
-import java.util.Map;
-
 import hugo.weaving.DebugLog;
-import rx.Observable;
-import rx.Subscriber;
-import rx.Subscription;
-import rx.android.schedulers.AndroidSchedulers;
-import rx.functions.Func0;
-import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity implements MainActivityContract.View {
-    public static final String TAG = MainActivity.class.getSimpleName();
     private MainActivityContract.Presenter presenter;
+
     @Override
     @DebugLog
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +21,8 @@ public class MainActivity extends AppCompatActivity implements MainActivityContr
     @Override
     protected void onDestroy() {
         super.onDestroy();
+
+        // Unsubscribing the subscription since activity got destroyed and may get memory leak
         presenter.unsubscribe();
     }
 
