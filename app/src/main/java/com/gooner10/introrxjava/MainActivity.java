e5257f0787409b0e8cd1cab1ebe9958cef45f09e
Birat Rai <biratrai@hotmail.com>
2019-07-11 07:59:24 -0400

    converting to use viewModel and databinging

--- app/src/main/java/com/gooner10/introrxjava/MainActivity.java
+++ app/src/main/java/com/gooner10/introrxjava/MainActivity.java
@@ -1,24 +1,40 @@
 package com.gooner10.introrxjava;
 
-import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.gooner10.introrxjava.databinding.ActivityMainBinding;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 import hugo.weaving.DebugLog;
 
-public class MainActivity extends AppCompatActivity implements MainActivityContract.View {
-    private MainActivityContract.Presenter presenter;
+public class MainActivity extends AppCompatActivity  {
+    private MainActivityViewModel activityViewModel;
     private ActivityMainBinding binding;
 
     @Override
     @DebugLog
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        presenter = new MainActivityPresenter(this);
-        presenter.subscribe();
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        activityViewModel = ViewModelProviders.of(this).get(MainActivityViewModel.class);
+        Button fetchButton = findViewById(R.id.fetch_gist);
+        fetchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                activityViewModel.getGistData().observe(MainActivity.this, new Observer<String>() {
+                    @Override
+                    public void onChanged(@Nullable String gist) {
+                        showData(gist);
+                    }
+                });
+            }
+        });
     }
 
     @Override
@@ -26,11 +42,10 @@ public class MainActivity extends AppCompatActivity implements MainActivityContr
         super.onDestroy();
 
         // Unsubscribing the subscription since activity got destroyed and may get memory leak
-        presenter.unsubscribe();
+        activityViewModel.unsubscribe();
     }
 
-    @Override
     public void showData(String gistData) {
-        binding.setGist(gistData);
+        binding.gistText.setText(gistData);
     }
 }
